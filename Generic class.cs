645ee6ee200624bd9d8c1f1a52d53f545f34503f
sadhana <40350669+SadhanaAnnadi@users.noreply.github.com>
2020-10-06 17:30:38 +0530

    Add files via upload

---
+++ Generic class.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericclass
+{
+    public class Example <T>
+    {
+        T data;
+        public Example(T check)
+        {
+            this.data = check;
+        }
+        public void write()
+        {
+            Console.WriteLine("the value passed is: " + this.data);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example<int> ex = new Example<int>(100);
+            Example<string> exp = new Example<string>("Hello World");
+            ex.write();
+            exp.write();
+            Console.ReadKey();
+
+        }
+    }
+}
