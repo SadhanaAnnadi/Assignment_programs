2db733aa40532b312baf93fd77fd4890d2378d96
sadhana <40350669+SadhanaAnnadi@users.noreply.github.com>
2020-10-09 15:48:36 +0530

    Add files via upload

---
+++ Multithreading_asgmt1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Multithreading_asgmt1
+{
+    class Program
+    {
+        public string Thread1(string str)
+        {
+            for(int i=1;i<=10;i++)
+            {
+                Console.WriteLine("This is Thread1 with position: " + i);
+            }
+            return str; 
+        }
+        public int Thread2(int val1,int val2)
+        {
+            Console.WriteLine("This is thread 2 starting");
+            Console.WriteLine("The sum value is {0}" , (val1 + val2));
+            Console.WriteLine("Thread 2 ended");
+            return val1+val2;
+        }
+        public string Thread3()
+        {
+            string str = "Thread 3 ended";
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine("Thread3 is in position: "+i);
+            }
+            Console.WriteLine(str);
+            return str ;
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Thread T1 = new Thread(()=>p.Thread1("Thread 1 ends here"));
+            
+            Thread T2 = new Thread(()=>p.Thread2(10,20));
+            Thread T3 = new Thread(()=>p.Thread3());
+            T1.Start(); T2.Start(); T3.Start();
+                       
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SlingleThread-Agnmt1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SingleThread_asgnt1
+{
+    class Program
+    {
+        public string  Thread1(string str)
+        {
+            for(int i=1;i<10;i++)
+            {
+                Console.WriteLine("This is Thread1 started");
+            }
+            return str;
+         }
+        public int Thread2(int val1,int val2)
+        {
+            
+            for(int i=1;i<10;i++)
+            {
+                Console.WriteLine("This is Thread2 started");
+                if (i==4)
+                {
+                    Console.WriteLine("thread2 will be in sleep for 10 sec");
+                    Thread.Sleep(10000);
+                }
+            }
+            Console.WriteLine("sum of two values is: ");
+            return val1 + val2;
+        }
+        
+        public string Thread3()
+        {
+            return "This is single thread 3";
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine(p.Thread1("Thread one ended"));
+            Console.WriteLine(p.Thread2(10,20));
+            Console.WriteLine(p.Thread3());
+            Console.ReadKey();
+
+        }
+    }
+}
