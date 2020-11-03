f8a02c7a6ed132dc386b98a9d82f8dd87d459aea
sadhana <40350669+SadhanaAnnadi@users.noreply.github.com>
2020-11-03 08:41:05 -0800

    Add files via upload

---
+++ ASP.Net/Design Patterns/Bridge/CitiPayment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeDP
+{
+   public interface IpaymentSystem //implementor interface
+    {
+        void ProcessPayment(string PaymentSystem);
+    }
+    public class CitiPaymentSystem:IpaymentSystem
+    {
+       public void ProcessPayment(string PaymentSystem) //concrete Implementor
+       {
+            Console.WriteLine("Using CitiBank gateWay for " + PaymentSystem);
+       }
+    }
+    public class IDBIPaymentSystem : IpaymentSystem
+    {
+        public void ProcessPayment(string PaymentSystem) //concrete implementor
+        {
+            Console.WriteLine("Using IDBIBank gateWay for " + PaymentSystem);
+        }
+    }
+}
---
+++ ASP.Net/Design Patterns/Bridge/Payment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeDP
+{
+    public abstract class Payment //abstraction
+    {
+        public IpaymentSystem _IpaymentSystem;
+        public abstract void MakePayment();
+        
+    }
+    public class CardPayment:Payment //refined Abstraction
+    {
+        public override void MakePayment()
+        {
+            _IpaymentSystem.ProcessPayment("CardPayment");
+
+        }
+    }
+    public class NetBankingPayment:Payment //refined Abstraction
+    {
+        public override void MakePayment()
+        {
+            _IpaymentSystem.ProcessPayment("NetBanking");
+
+        }
+    }
+}
