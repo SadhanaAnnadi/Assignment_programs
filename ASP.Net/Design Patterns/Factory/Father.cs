ad5076607cfef6a38693899b63df3c13a85b07d7
sadhana <40350669+SadhanaAnnadi@users.noreply.github.com>
2020-11-03 08:38:54 -0800

    Add files via upload

---
+++ ASP.Net/Design Patterns/Factory/Father.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDP
+{
+    public abstract class Father
+    {
+        protected abstract Fruit GetFruit();//interface method for creating object
+        public void Makejuice()
+        {
+            var fruit = GetFruit();
+            Console.WriteLine("The juice has been made using the fruit : " + fruit.GetType().Name);
+        }
+        
+    }
+    public class Kid1:Father//sub class to decide the instance
+    {
+        protected override Fruit GetFruit()
+        {
+
+            return new Apple();
+        }
+    }
+    public class Kid2:Father
+    {
+        protected override Fruit GetFruit()
+        {
+            return new orange();
+        }
+
+    }
+}
---
+++ ASP.Net/Design Patterns/Factory/Fruit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDP
+{
+    public class Fruit
+    {
+
+    }
+    public class Apple:Fruit
+    {
+
+    }
+    public class orange:Fruit
+    {
+
+    }
+}
