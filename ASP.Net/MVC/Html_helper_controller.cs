a623af4c53137324af97a83ce5e96b448c174dbc
sadhana <40350669+SadhanaAnnadi@users.noreply.github.com>
2020-11-12 06:25:23 -0800

    Create Html_helper_controller.cs

---
+++ ASP.Net/MVC/Html_helper_controller.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace HtmlHelper_Asgmt1.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
