bce16ffb4bb9b201d2e23c438b12c85d6c65f2af
sadhana <40350669+SadhanaAnnadi@users.noreply.github.com>
2020-11-16 03:13:55 -0800

    Add files via upload

---
+++ ASP.Net/MVC/CodeFirst_Approach/Student1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Entity_ReAssignment1.Models
+{
+    public class Student1 : DbContext
+    {
+        public Student1() : base("StudentConnectionString")
+        {
+
+        }
+        public DbSet<Student_details1>STable { get; set; }
+    }
+}
---
+++ ASP.Net/MVC/CodeFirst_Approach/Student_details1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entity_ReAssignment1.Models
+{
+    [Table("STable")]
+    public class Student_details1
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public long Phoneno { get; set; }
+        public string City { get; set; }
+
+    }
+}
