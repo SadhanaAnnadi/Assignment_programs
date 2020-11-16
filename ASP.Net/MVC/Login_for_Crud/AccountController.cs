28788b51afc74bafc0346cd9fca39e97f213b083
sadhana <40350669+SadhanaAnnadi@users.noreply.github.com>
2020-11-16 05:15:21 -0800

    Add files via upload

---
+++ ASP.Net/MVC/Login_for_Crud/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Authentication_MVC.Models;
+using System.Web.Security;
+
+namespace Authentication_MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using (var contex = new EmployeeDBEntities())
+            {
+                bool isValid = contex.User.Any(x => x.UserName == model.UserName && x.Password == model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Employee2");
+                }
+                ModelState.AddModelError("", "Invalid Username or Password");
+
+                return View();
+            }
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using (var context = new EmployeeDBEntities())
+            {
+                context.User.Add(model);
+                context.SaveChanges();
+            }
+                return RedirectToAction("Login");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
